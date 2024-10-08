048a355aba9531f22d0f16438a11241a8fcbbdc5
ALAMIN037 <botmim03@gmail.com>
2024-10-09 02:58:40 +0300

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, threadsData, getLang, message }) {
-    const supportGroupThreadID = "8164226533705433"; // Replace with your support group thread ID
+    const supportGroupThreadID = "26997806536533074"; // Replace with your support group thread ID
     const botID = api.getCurrentUserID();
 
     try {
